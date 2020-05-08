0a8c32fba43b75bab86cffd918073742de440637
Tytraman <tytraman@gmail.com>
2020-05-08 16:51:38 +0200

    plugin yml bug

---
+++ src/main/java/no/tytraman/newguilds/NewGuilds.java
@@ -0,0 +1,12 @@
+package no.tytraman.newguilds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NewGuilds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
